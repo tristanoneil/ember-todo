7f5e6ac6293ecc641c5d924ca2dfc3641b375c91
Tristan O'Neil <tristanoneil@gmail.com>
2014-05-11 09:27:35 -0400

    Allow todos to be marked as done

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -2,6 +2,12 @@ export default Ember.ObjectController.extend({
   actions: {
     removeTodo: function() {
       this.get('model').deleteRecord();
+    },
+
+    markDone: function() {
+      var todo = this.get('model');
+      todo.set('done', !todo.get('done'));
+      todo.save();
     }
   }
 });
